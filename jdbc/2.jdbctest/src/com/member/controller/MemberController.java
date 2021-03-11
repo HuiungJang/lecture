70b4a5b9a7140649a548df9a2eb40a4fc5491f5d
horlllla <44770275+HuiungJang@users.noreply.github.com>
2021-03-11 22:15:23 +0900

    JDBC 수업 2

--- jdbc/2.jdbctest/src/com/member/controller/MemberController.java
+++ jdbc/2.jdbctest/src/com/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.member.model.dao.MemberDao;
 import com.member.model.vo.Member;
 import com.member.view.MemberView;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class MemberController {
@@ -13,14 +14,82 @@ public class MemberController {
     public void mainMenu(){
         // 메인메뉴를 호출하는 기능.
         view = new MemberView();
-        view.mainMenu();
+        view.mainMenu(this);
     }
 
-    public List<Member> selectAll(){
-        // DB student 계정의 member테이블에 있는 전체 데이터 가져오는 서비스
+//    public List<Member> selectAll() {
+//        // DB student 계정의 member테이블에 있는 전체 데이터 가져오는 서비스
+//
+//        // view에서 받은 데이터 출력하기 -> 숙제
+//        // 내가한거
+//        return dao.selectAll();
+//
+//    }
 
-        // view에서 받은 데이터 출력하기 -> 숙제
-        return dao.selectAll();
+    public void selectAll() {
+        // 선생님이 한거
+        //DB student계정의 member테이블에 있는 전체 데이터 가져오는 서비스
+        List<Member> result = dao.selectAll();
+        //view에서 받은 데이터 출력하기 -> 오늘 숙제
+        //System.out.println(result);
+//		for(Member m : result) {
+//			System.out.println(m);
+//		}
+//		result.get(0).setMemberName("박찬혁");
+//		for(Member m : result) {
+//			System.out.println(m);
+//		}
+        //view에서 데이터를 출력하는 매소드를 호출
+        //console창에 전달받은 데이터를 출력해주고 마무의ㄹㅣ!
+        System.out.println(view);
+        view.printData(result);
+    }
+
+
+//    public void searchId(){
+//         내가한거
+//        // 1. 검색할 아이디를 입력받는다 -> view에 요청
+//        // 2. 입력받은 값을 받으면 db에 있는지 확인한다. -> dao
+//        // 3. dao가 준 결과를 view에서 출력한다. -> view에 요청
+//        String memberId = view.inputData();
+//        List<Member> result = dao.searchId(memberId);
+//
+//        if(result == null) {
+//            System.out.println("찾는 회원이 없습니다.");
+//        }else{
+//            view.printData(result);
+//        }
+//    }
+
+    public void searchId(){
+        // 선생님이 한거
+        // 1. 검색할 아이디를 입력받는다 -> view에 요청
+        // 2. 입력받은 값을 받으면 db에 있는지 확인한다. -> dao
+        // 3. dao가 준 결과를 view에서 출력한다. -> view에 요청
+
+        String memberId = view.searchData();
+        Member m = dao.searchId(memberId);
+
+        // Arrays.asList(new Member[] {m});
+        // 일반배욜을 List로 변경해주는 메소드
+        view.printData(m);
 
     }
+
+    public void searchName(){
+        String memberName = view.searchData();
+        List<Member> l = dao.searchName(memberName);
+        if(l == null){
+            System.out.println("일치하는 이름이 없습니다.");
+        }else {
+            view.printData(l);
+        }
+    }
+
+    public void enrollMember(){
+        Member inputData = view.inputData();
+        int result = dao.insertMember(inputData);
+        view.printMsg( result>0? "회원 등록완료" : "회원등록 실패");
+    }
+
 }
--- jdbc/2.jdbctest/src/com/member/model/dao/MemberDao.java
+++ jdbc/2.jdbctest/src/com/member/model/dao/MemberDao.java
@@ -18,8 +18,6 @@ public class MemberDao {
 
         // 반환형에 대한 선언
         List<Member> result = new ArrayList<>();
-        Member mb = new Member();
-
 
         try{
             Class.forName("oracle.jdbc.driver.OracleDriver");
@@ -30,38 +28,44 @@ public class MemberDao {
             String memInfo = "select * from MEMBER";
             rs = stmt.executeQuery(memInfo);
 
+//            while(rs.next()){
+//
+//                String memberId = rs.getString("MEMBER_ID");
+//                String memberPwd = rs.getString("MEMBER_PWD");
+//                String memberName = rs.getString("MEMBER_NAME");
+//                String gender = rs.getString("GENDER");
+//                int age = rs.getInt("AGE");
+//                String email = rs.getString("EMAIL");
+//                String phone = rs.getString("PHONE");
+//                String address = rs.getString("ADDRESS");
+//                String hobby = rs.getString("HOBBY");
+//                Date enrollDate =rs.getDate("ENROLL_DATE");
+//
+//                // 가져온 데이터 result 객체에 넣기
+//                // 숙제
+//                // 내가한거
+//
+//                result.add(new Member(memberId,memberPwd,memberName,gender,
+//                                    age,email,phone,address,hobby,enrollDate));
+//            }
+
+            // row는 하나의 객체
             while(rs.next()){
+                // 선생님이 한거.
+                Member mb = new Member();
 
-                String memberId = rs.getString("MEMBER_ID");
-                String memberPwd = rs.getString("MEMBER_PWD");
-                String memberName = rs.getString("MEMBER_NAME");
-                String gender = rs.getString("GENDER");
-                int age = rs.getInt("AGE");
-                String email = rs.getString("EMAIL");
-                String phone = rs.getString("PHONE");
-                String address = rs.getString("ADDRESS");
-                String hobby = rs.getString("HOBBY");
-                Date enrollDate =rs.getDate("ENROLL_DATE");
-//                System.out.println(memberId+" "+memberPwd+" "+memberName+" "+gender+" "+age+" "+
-//                                    email+" "+phone+" "+address+" "+hobby+" "+enrollDate);
+                mb.setMemberId(rs.getString("MEMBER_ID"));
+                mb.setMemberPwd(rs.getString("MEMBER_PWD"));
+                mb.setMemberName(rs.getString("MEMBER_NAME"));
+                mb.setGender(rs.getString("GENDER"));
+                mb.setAge(rs.getInt("AGE"));
+                mb.setEmail(rs.getString("EMAIL"));
+                mb.setPhone(rs.getString("PHONE"));
+                mb.setAddress(rs.getString("ADDRESS"));
+                mb.setHobby(rs.getString("HOBBY"));
+                mb.setEnrollDate(rs.getDate("ENROLL_DATE"));
 
-
-
-
-                // 가져온 데이터 result 객체에 넣기
-                // 숙제
-                mb.setMemberId(memberId);
-                mb.setMemberPwd(memberPwd);
-                mb.setMemberName(memberName);
-                mb.setGender(gender);
-                mb.setAge(age);
-                mb.setEmail(email);
-                mb.setPhone(phone);
-                mb.setAddress(address);
-                mb.setHobby(hobby);
-                mb.setEnrollDate(enrollDate);
-
-                result.add(new Member(memberId,memberPwd,memberName,gender,age,email,phone,address,hobby,enrollDate));
+                result.add(mb);
             }
 
         }catch (ClassNotFoundException e){
@@ -80,4 +84,229 @@ public class MemberDao {
 
         return result;
     }
+
+//    public List<Member> searchId(String memberId){
+//      내가한거
+//        try {
+//
+//            Class.forName("oracle.jdbc.driver.OracleDriver");
+//            conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe"
+//                                    ,"student","student");
+//            stmt = conn.createStatement();
+//            String sql = "SELECT * FROM MEMBER WHERE MEMBER_ID ='"+memberId+"'";
+//
+//            rs = stmt.executeQuery(sql);
+//
+//            List<Member> result = new ArrayList<>();
+//            while(rs.next()) {
+//                Member mb = new Member();
+//
+//                mb.setMemberId(rs.getString("MEMBER_ID"));
+//                mb.setMemberPwd(rs.getString("MEMBER_PWD"));
+//                mb.setMemberName(rs.getString("MEMBER_NAME"));
+//                mb.setGender(rs.getString("GENDER"));
+//                mb.setAge(rs.getInt("AGE"));
+//                mb.setEmail(rs.getString("EMAIL"));
+//                mb.setPhone(rs.getString("PHONE"));
+//                mb.setAddress(rs.getString("ADDRESS"));
+//                mb.setHobby(rs.getString("HOBBY"));
+//                mb.setEnrollDate(rs.getDate("ENROLL_DATE"));
+//
+//                result.add(mb);
+//                return  result;
+//            }
+//
+//        }catch (ClassNotFoundException e){
+//            e.printStackTrace();
+//        }catch (SQLException e){
+//            e.printStackTrace();
+//        }finally {
+//            try {
+//                if (!rs.isClosed() && rs != null) rs.close();
+//                if (!stmt.isClosed() && stmt != null) stmt.close();
+//                if (!conn.isClosed() && conn != null) conn.close();
+//
+//            }catch (SQLException e){
+//                e.printStackTrace();
+//            }
+//
+//        }
+//
+//        return null;
+//    }
+
+
+    public Member searchId(String memberId){
+        Member m =null;
+        try {
+
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe"
+                    ,"student","student");
+            stmt = conn.createStatement();
+            String sql = "SELECT * FROM MEMBER WHERE MEMBER_ID ='"+memberId+"'";
+
+            rs = stmt.executeQuery(sql);
+
+            while(rs.next()) {
+                m= new Member();
+                m.setMemberId(rs.getString("MEMBER_ID"));
+                m.setMemberPwd(rs.getString("MEMBER_PWD"));
+                m.setMemberName(rs.getString("MEMBER_NAME"));
+                m.setGender(rs.getString("GENDER"));
+                m.setAge(rs.getInt("AGE"));
+                m.setEmail(rs.getString("EMAIL"));
+                m.setPhone(rs.getString("PHONE"));
+                m.setAddress(rs.getString("ADDRESS"));
+                m.setHobby(rs.getString("HOBBY"));
+                m.setEnrollDate(rs.getDate("ENROLL_DATE"));
+            }
+
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }finally {
+            try {
+                if (!rs.isClosed() && rs != null) rs.close();
+                if (!stmt.isClosed() && stmt != null) stmt.close();
+                if (!conn.isClosed() && conn != null) conn.close();
+
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
+
+        }
+
+        return m;
+    }
+
+
+    public List<Member> searchName(String memberName){
+       List<Member> l = new ArrayList<>();
+        try{
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe"
+                                ,"student","student");
+            stmt = conn.createStatement();
+
+            String sql = "SELECT * FROM MEMBER WHERE MEMBER_NAME LIKE '%"+memberName+"%'";
+
+            rs = stmt.executeQuery(sql);
+
+
+            while(rs.next()){
+               Member m = new Member();
+
+//                m.setMemberId(rs.getString("MEMBER_ID"));
+//                m.setMemberPwd(rs.getString("MEMBER_PWD"));
+//                m.setMemberName(rs.getString("MEMBER_NAME"));
+//                m.setGender(rs.getString("GENDER"));
+//                m.setAge(rs.getInt("AGE"));
+//                m.setEmail(rs.getString("EMAIL"));
+//                m.setPhone(rs.getString("PHONE"));
+//                m.setAddress(rs.getString("ADDRESS"));
+//                m.setHobby(rs.getString("HOBBY"));
+//                m.setEnrollDate(rs.getDate("ENROLL_DATE"));
+
+                m.setMemberId(rs.getString(1));
+                m.setMemberPwd(rs.getString(2));
+                m.setMemberName(rs.getString(3));
+                m.setGender(rs.getString(4));
+                m.setAge(rs.getInt(5));
+                m.setEmail(rs.getString(6));
+                m.setPhone(rs.getString(7));
+                m.setAddress(rs.getString(8));
+                m.setHobby(rs.getString(9));
+                m.setEnrollDate(rs.getDate(10));
+                // 이렇게 인덱스로 넣을 수도 있다.
+                /*
+
+                인덱스로 했을 때
+
+                장점
+                1. 입력하기 편하다.
+
+                단점
+                1. 컬럼 인덱스가 정확이 어떤 컬럼을 가르키는지 알기 어렵다.
+                2. 컬럼의 순서가 바뀌면 원하는 값이 출력되지 않는다.
+
+                => 컬럼명을 쓰는 것이 좋지만 코딩 스타일마다 다르기 때문에
+                    잘 알아보고 하자.
+
+
+                 */
+
+
+
+                l.add(m);
+            }
+
+
+        }catch(ClassNotFoundException e){
+            e.printStackTrace();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }finally {
+            try{
+                if(!rs.isClosed() && rs != null) rs.close();
+                if(!stmt.isClosed() && stmt != null) stmt.close();
+                if(!conn.isClosed() && conn != null) conn.close();
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
+
+        return l;
+    }
+
+    public int insertMember(Member m){
+        int result=0;
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe"
+                    , "student", "student");
+            stmt = conn.createStatement();
+
+            conn.setAutoCommit(false);
+            // 트랜젝션 처리(commit, rollback) 개발자가 설정할 수 있게함
+
+            String sql = "INSERT INTO MEMBER VALUES(" +
+                    "'"+m.getMemberId()
+                    + "','"+m.getMemberPwd()
+                    +"','"+m.getMemberName()
+                    +"','"+m.getGender()
+                    +"',"+m.getAge()
+                    +",'"+ m.getEmail()
+                    +"','"+m.getPhone()
+                    +"','"+m.getAddress()
+                    +"','"+m.getHobby()
+                    +"','"+m.getEnrollDate()
+                    +"'"
+                    +")";
+
+            result = stmt.executeUpdate(sql);
+            // insert 트랜젝션 처리해야함.
+            // Connection 객체에 트랜젝션을 처리해주는 메소드가 있음.
+            // commit() / rollback() 메소드
+            if(result>0) conn.commit();
+            else conn.rollback();
+
+
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }finally {
+            try{
+                if(!stmt.isClosed() && stmt != null) stmt.close();
+                if(!conn.isClosed() && conn != null) conn.close();
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
+
+        return result;
+    }
+
 }
