dc20d67da2ae1e1bca07de21b398d3e04e205c56
HuiungJang <jhw1345@gmail.com>
2021-04-14 10:25:25 +0900

    #04 plus연산으로 전환, 출력구문 전환-HU-

--- gitWork/src/com/git/controller/GitController.java
+++ gitWork/src/com/git/controller/GitController.java
@@ -5,6 +5,7 @@ public class GitController {
         System.out.println("프린트하는 것");
     }
     public int calcPlus(int su1, int su2){
-        return su1-su2;
+        System.out.println(su1+su2);
+        return su1+su2;
     }
 }
