0a5ef4580f8e087f08bc3e7692e83009e1f5596c
horlllla <44770275+HuiungJang@users.noreply.github.com>
2021-05-03 22:45:36 +0900

    회원정보수정

---
+++ Servlet/ROOT/_basicservlet/_06_helloMVC/src/main/java/com/member/controller/MemberReplaceServlet.java
@@ -0,0 +1,48 @@
+package com.member.controller;
+
+import com.member.model.service.Service;
+import com.member.model.vo.Member;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import javax.websocket.Session;
+import java.io.IOException;
+
+@WebServlet(name = "MemberReplaceServlet", value = "/memberView.do")
+public class MemberReplaceServlet extends HttpServlet {
+    private static final long serialVersionUID = -2082680917646969575L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 클라이언트가 보낸 아이디를 기준으로 정보를 가져옴
+
+        String userId = request.getParameter("userId");
+        Member m = new Service().checkDuplicateId(userId);
+
+        // m = null 이면 회원정보 수정불가(일치하는 회원이 없음)
+        // msg페이지로 이동알림 메세지를 출력하고 로그인을 취소하고 메인화면으로 이동
+        // m != null 이면 회원정보 수정으로 들어감
+
+        String view="";
+        String loc ="/logout";
+        String msg="";
+        if(m==null){
+            msg="가입된 회원이 아닙니다.";
+            view="/views/common/msg.jsp";
+        }else{
+            view = "/views/member/replaceMemberInfo.jsp";
+        }
+        request.setAttribute("msg",msg);
+        request.setAttribute("loc",loc);
+        request.setAttribute("member",m);
+
+        request.getRequestDispatcher(view).forward(request,response);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
