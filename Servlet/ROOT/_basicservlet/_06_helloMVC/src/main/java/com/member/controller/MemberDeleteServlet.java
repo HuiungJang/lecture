6d2400f4dd93c7251330779edc61f9106c6a2cf0
horlllla <44770275+HuiungJang@users.noreply.github.com>
2021-05-05 21:35:58 +0900

    회원삭제_선생님이 한 로직 추가

--- Servlet/ROOT/_basicservlet/_06_helloMVC/src/main/java/com/member/controller/MemberDeleteServlet.java
+++ Servlet/ROOT/_basicservlet/_06_helloMVC/src/main/java/com/member/controller/MemberDeleteServlet.java
@@ -13,23 +13,37 @@ public class MemberDeleteServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setCharacterEncoding("UTF-8");
+        // 내가한거
+//        request.setCharacterEncoding("UTF-8");
+//
+//        String id = request.getParameter("userId");
+//
+//        int result = new Service().deleteMember(id);
+//
+//        request.setAttribute("msg",result>0?"회원탈퇴가 완료되었습니다.":"회원탈퇴 실패");
+//        request.setAttribute("loc","/");
+//        HttpSession session =request.getSession();
+//        session.invalidate();
+//
+//        request.getRequestDispatcher("").forward(request,response);
 
-        String id = request.getParameter("userId");
+        // 클라이언트가 보낸 id 값(PK값)을 기준으로 조회함
+        // 샘이 한거
+        String userId = request.getParameter("userId");
+        int result = new Service().deleteMember("userId");
 
-        int result = new Service().deleteMember(id);
+        String msg = result>0?"정상적으로 탈퇴되었습니다.":"탈퇴에 실패했습니다. 다시 시도해주세요";
+        String loc = result>0?"logout":"/memberView.do";
 
-        request.setAttribute("msg",result>0?"회원탈퇴가 완료되었습니다.":"회원탈퇴 실패");
-        request.setAttribute("loc","/");
-        HttpSession session =request.getSession();
-        session.invalidate();
+        request.setAttribute("msg",msg);
+        request.setAttribute("loc",loc);
 
-        request.getRequestDispatcher("").forward(request,response);
+        request.getRequestDispatcher("/views/common/msg.jsp").forward(request,response);
 
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request , HttpServletResponse response) throws ServletException, IOException {
         doGet(request, response);
     }
 }
